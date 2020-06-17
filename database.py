d4f45d6cbbfeda97c74595a6669ffd45b0344439
taisei <tjt@berkeley.edu>
2020-06-16 23:11:14 -0700

    create db

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
-# import sqlite3
-# from sqlite3 import Error
+import sqlite3
+from sqlite3 import Error
 
-# def create_connection(db):
-#     """ create a db connection to SQLite db """
-#     conn = None
-#     try:
-#         conn = sqlite3.connect(db)
-#         print(sqlite3.version)
-#     except Error as e:
-#         print(e)
-#     finally:
-#         if conn:
-#             conn.close()
+def create_connection(db):
+    """ create a db connection to SQLite db """
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
-# if __name__=='main':
-#     create_connection(r"pythonsqlite.db")
+if __name__=='main':
+    create_connection(r"pythonsqlite.db")
