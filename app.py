da7a947d79a462bc38639a57ad29ad36c60bedae
taisei <tjt@berkeley.edu>
2020-06-24 20:45:46 -0700

    app = MVP

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+from random import randint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ATLA.db'
@@ -11,10 +12,15 @@ class Quotes(db.Model):
     author = db.Column(db.String(50), nullable=False)
     text = db.Column(db.String(200), nullable=False)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
 def main_page():
-    names = Quotes.query.first()
-    return render_template('index.html', names=names)
+    random_int = randint(1, 54)
+    if request.method == 'POST':
+        quote = Quotes.query.filter_by(id = str(random_int)).first()
+        return render_template('index.html', quote=quote)
+    else:
+        quote = Quotes.query.filter_by(id = '5').first()
+        return render_template('index.html', quote=quote)
 
 if __name__ == '__main__':
     app.run(debug=True)
