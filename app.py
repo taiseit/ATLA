8c616a4c2dc02962b8e9428a8bc64725f132b4d9
taisei <tjt@berkeley.edu>
2020-06-28 21:53:58 -0700

    fully implement app logic

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ class Quotes(db.Model):
     text = db.Column(db.String(200), nullable=False)
 
 array_index = 0
-count = 10 # Quotes.query.count()
-scramble_index = random.sample(range(1, count + 1), count)
+count = Quotes.query.count()
+shuffle_index = random.sample(range(1, count + 1), count)
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
     def helper():
         global array_index
+        x = request.args.get('button')
+        print(str(x))
         # reset index 
-        if abs(array_index) > len(scramble_index) - 2:
+        if abs(array_index) > len(shuffle_index) - 2:
             array_index = 0
         # next button 
-        # requires a way to detect the right button is clicked
-        elif request.method == 'POST':
-            array_index += 1
+        elif request.method == 'POST' and request.form.get('submit_button') == 'next-button':
+                array_index += 1
         # previous button
-        # requires a way to detect the left button is clicked 
-        else:
+        elif request.method == 'POST' and request.form.get('submit_button') == 'prev-button':
             array_index -= 1
-        quote_index = str(scramble_index[array_index])
+        quote_index = str(shuffle_index[array_index])
         quote = Quotes.query.filter_by(id = quote_index).first()
         return render_template('index.html', quote=quote)
     return helper()
